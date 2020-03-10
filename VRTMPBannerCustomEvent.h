8bd6d99dfd1218d5557a1aa7ac5d4e34085fdd50
Scott McCoy <scotthmccoy@gmail.com>
2020-03-10 11:56:03 -0700

    fix: Import issues

--- VRTMPBannerCustomEvent.h
+++ VRTMPBannerCustomEvent.h
@@ -1,6 +1,14 @@
 #import <Foundation/Foundation.h>
-@import MoPub;
 #import <VrtcalSDK/VrtcalSDK.h>
+
+#if __has_include(<MoPub/MoPub.h>)
+#import <MoPub/MoPub.h>
+#elif __has_include(<MoPubSDKFramework/MoPub.h>)
+#import <MoPubSDKFramework/MoPub.h>
+#else
+#import "MPBannerCustomEvent.h"
+#endif
+
 
 @interface VRTMPBannerCustomEvent : MPBannerCustomEvent <VRTBannerDelegate>
 @property (strong, nonatomic) VRTBanner *vrtBanner;
